8a18e90f38cbeec44f0a9da9d75915dcee1e7f60
Ian McGillivary <wiian16@icloud.com>
2025-11-20 14:36:21 -0500

    Add disk partitioning to module 0

--- modules/00-partition.sh
+++ modules/00-partition.sh
@@ -1,6 +1,37 @@
 #!/usr/bin/env bash
 
-module_00() {
-    error "Module not implemented yet"
+# Setup disk or partition for installation
+
+partition_disk() {
+    warn "Beginning disk partition, this will erase ALL data on the disk"
+    if ! confirm; then
+        exit 1
+    fi
+
+    info "Formatting drive "
+
+    run parted /dev/"$INSTALL_LOCATION" --script mklabel gpt
+    run parted /dev/"$INSTALL_LOCATION" --script mkpart ESP fat32 1MiB 1GiB
+    run parted /dev/"$INSTALL_LOCATION" --script set 1 boot on
+    run parted /dev/"$INSTALL_LOCATION" --script mkpart primary btrfs 1GiB 100%
+}
+
+partition_part() {
+    error "Function not implemented yet"
     return 1
+}
+
+module_00() {
+    case $INSTALL_TYPE in
+    drive)
+        partition_disk
+        ;;
+    partition)
+        partition_part
+        ;;
+    *)
+        error "Internal error: bad install location in module 0 ($INSTALL_TYPE)"
+        false
+        ;;
+    esac
 }
