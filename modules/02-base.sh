5161e807993a4735d54a61e33c581164fd963fd1
Ian McGillivary <wiian16@icloud.com>
2025-11-21 13:02:10 -0500

    Add pacstrap and genfstab

--- modules/02-base.sh
+++ modules/02-base.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
 module_02() {
-    error "Module not implemented yet"
-    return 1
+    # Run pacstrap
+    info "Running pacstrap to install base system"
+    run pacstrap -K /mnt ${CORE_PACKAGES[@]}
+
+    # Generate fstab entries
+    info "Generating fstab entries"
+    run genfstab -U /mnt >>/mnt/etc/fstab
 }
