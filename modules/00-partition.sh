831f88ec2e0b452fa8c5980bb3e10fd0a23490c7
Ian McGillivary <wiian16@icloud.com>
2025-11-20 21:13:40 -0500

    Format and mount formatted partitions

--- modules/00-partition.sh
+++ modules/00-partition.sh
@@ -8,12 +8,42 @@ partition_disk() {
         exit 1
     fi
 
-    info "Formatting drive "
+    info "Formatting drive"
 
+    # Create partition table and partitions
     run parted /dev/"$INSTALL_LOCATION" --script mklabel gpt
     run parted /dev/"$INSTALL_LOCATION" --script mkpart ESP fat32 1MiB 1GiB
     run parted /dev/"$INSTALL_LOCATION" --script set 1 boot on
     run parted /dev/"$INSTALL_LOCATION" --script mkpart primary btrfs 1GiB 100%
+
+    local efi_partition
+    local root_partition
+
+    # Set efi and root partition names
+    if [[ $INSTALL_LOCATION == "nvme*" ]]; then
+        efi_partition="/dev/${INSTALL_LOCATION}p1"
+        root_partition="/dev/${INSTALL_LOCATION}p2"
+    else
+        efi_partition="/dev/${INSTALL_LOCATION}1"
+        root_partition="/dev/${INSTALL_LOCATION}2"
+    fi
+
+    info "Created EFI parititon at $efi_partition"
+    info "Created root partition at $root_partition"
+
+    # Format partitions
+    info "Formatting partitions"
+    run mkfs.fat -F32 "$efi_partition"
+    # TODO: may not need to format btrfs, seems to be done by parted (added -f flag to avoid error)
+    run mkfs.btrfs -f "$root_partition"
+
+    # Mount partitions
+    info "Mounting partitions"
+
+    run ensure_dir /mnt/boot/efi
+
+    run mount "$root_partition" /mnt
+    run mount "$efi_partition" /mnt/boot/efi
 }
 
 partition_part() {
