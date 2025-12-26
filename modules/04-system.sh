660df5f6e9afda3dc7cfa60fab48421050a33b10
Ian McGillivary <wiian16@icloud.com>
2025-12-26 17:44:36 -0600

    Change username for zsh installation

--- modules/04-system.sh
+++ modules/04-system.sh
@@ -64,7 +64,7 @@ module_04() {
 
     # Run install scripts for regular user and root
     run_chroot sh -c "RUNZSH=no CHSH=no $(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-    run_chroot su - wiian -c "RUNZSH=no CHSH=no $(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    run_chroot su - $USERNAME -c "RUNZSH=no CHSH=no $(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
     run_chroot chsh root -s /usr/bin/zsh
     run_chroot chsh $USERNAME -s /usr/bin/zsh
